b710ea819cf5e2c74744fc69d8024fbbf09128e6
wilsonf <feras.wilson@ipsoft.com>
2019-10-07 19:13:52 +0200

    Add tests

--- src/test/java/com/dopplertask/doppler/ExecutionServiceTest.java
+++ src/test/java/com/dopplertask/doppler/ExecutionServiceTest.java
@@ -7,6 +7,7 @@ import com.dopplertask.doppler.domain.TaskExecution;
 import com.dopplertask.doppler.service.ExecutionServiceImpl;
 import com.dopplertask.doppler.service.TaskExecutionRequest;
 import com.dopplertask.doppler.service.TaskServiceImpl;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,10 +57,7 @@ public class ExecutionServiceTest {
         when(taskExecutionDao.findById(eq(10L))).thenReturn(executionOptional);
 
         // Run test with data
-        TaskExecutionRequest request = new TaskExecutionRequest();
-        request.setTaskName("ExampleTask");
-        request.setExecutionId(10L);
-        request.setParameters(new HashMap<>());
+        TaskExecutionRequest request = createTaskRequest("ExampleTask", null, 10L);
 
         TaskExecution resultExecution = executionService.startExecution(request, taskService);
 
@@ -86,16 +84,104 @@ public class ExecutionServiceTest {
         when(taskDao.findById(eq(20L))).thenReturn(exampleTaskOptional);
         when(taskExecutionDao.findById(eq(10L))).thenReturn(executionOptional);
 
-        // Run test with data
-        TaskExecutionRequest request = new TaskExecutionRequest();
-        request.setTaskName("ExampleTask");
-        request.setExecutionId(10L);
-        request.setParameters(new HashMap<>());
-
         TaskExecution resultExecution = executionService.processActions(exampleTask.getId(), execution.getId(), null);
 
         // Check that we have everything correct
         Assert.assertEquals(10L, resultExecution.getId().longValue());
         Assert.assertEquals(exampleTask, resultExecution.getTask());
+    }
+
+    @Test
+    public void testStartExecutionByChecksumAndTaskNameShouldReturnExecution() {
+        // Prepare Task
+        Task exampleTask = new Task();
+        exampleTask.setName("ExampleTask");
+        exampleTask.setChecksum("123123");
+        Optional<Task> exampleTaskOptional = Optional.of(exampleTask);
+
+        // Prepare Execution
+        TaskExecution execution = new TaskExecution();
+        execution.setId(10L);
+        Optional<TaskExecution> executionOptional = Optional.of(execution);
+
+        // Assign values
+        when(taskDao.findFirstByNameOrderByCreatedDesc(eq("ExampleTask"))).thenReturn(exampleTaskOptional);
+        when(taskDao.findFirstByChecksumStartingWith(eq("123123"))).thenReturn(exampleTaskOptional);
+        when(taskExecutionDao.findById(eq(10L))).thenReturn(executionOptional);
+
+        // Run test with data
+        TaskExecutionRequest request = createTaskRequest("ExampleTask", "123123", 10L);
+
+        TaskExecution resultExecution = executionService.startExecution(request, taskService);
+
+        // Check that we have everything correct
+        Assert.assertEquals(10L, resultExecution.getId().longValue());
+        Assert.assertEquals(exampleTask, resultExecution.getTask());
+    }
+
+    @Test
+    public void testStartExecutionByChecksumShouldReturnExecution() {
+        // Prepare Task
+        Task exampleTask = new Task();
+        exampleTask.setName("TaskName");
+        exampleTask.setChecksum("123123");
+        Optional<Task> exampleTaskOptional = Optional.of(exampleTask);
+
+        // Prepare Execution
+        TaskExecution execution = new TaskExecution();
+        execution.setId(10L);
+        Optional<TaskExecution> executionOptional = Optional.of(execution);
+
+        // Assign values
+        when(taskDao.findFirstByNameOrderByCreatedDesc(eq("ExampleTask"))).thenReturn(exampleTaskOptional);
+        when(taskDao.findFirstByChecksumStartingWith(eq("123123"))).thenReturn(exampleTaskOptional);
+        when(taskExecutionDao.findById(eq(10L))).thenReturn(executionOptional);
+
+        // Run test with data
+        TaskExecutionRequest request = createTaskRequest("123123", "123123", 10L);
+
+        TaskExecution resultExecution = executionService.startExecution(request, taskService);
+
+        // Check that we have everything correct
+        Assert.assertEquals(10L, resultExecution.getId().longValue());
+        Assert.assertEquals(exampleTask, resultExecution.getTask());
+    }
+
+    @Test
+    public void testStartExecutionByTaskNameShouldReturnExecution() {
+        // Prepare Task
+        Task exampleTask = new Task();
+        exampleTask.setName("ExampleTask");
+        exampleTask.setChecksum("123123");
+        Optional<Task> exampleTaskOptional = Optional.of(exampleTask);
+
+        // Prepare Execution
+        TaskExecution execution = new TaskExecution();
+        execution.setId(10L);
+        Optional<TaskExecution> executionOptional = Optional.of(execution);
+
+        // Assign values
+        when(taskDao.findFirstByNameOrderByCreatedDesc(eq("ExampleTask"))).thenReturn(exampleTaskOptional);
+        when(taskDao.findFirstByChecksumStartingWith(eq("123123"))).thenReturn(exampleTaskOptional);
+        when(taskExecutionDao.findById(eq(10L))).thenReturn(executionOptional);
+
+        // Run test with data
+        TaskExecutionRequest request = createTaskRequest("ExampleTask", "ExampleTask", 10L);
+
+        TaskExecution resultExecution = executionService.startExecution(request, taskService);
+
+        // Check that we have everything correct
+        Assert.assertEquals(10L, resultExecution.getId().longValue());
+        Assert.assertEquals(exampleTask, resultExecution.getTask());
+    }
+
+    private TaskExecutionRequest createTaskRequest(String taskName, String checksum, long executionId) {
+        TaskExecutionRequest request = new TaskExecutionRequest();
+        request.setTaskName(taskName);
+        request.setChecksum(checksum);
+        request.setExecutionId(executionId);
+        request.setParameters(new HashMap<>());
+
+        return request;
     }
 }
